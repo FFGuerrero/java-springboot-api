89206ac3b35a1821395b74455135c62fbf849e90
Freddy Guerrero <ffguerrero01@gmail.com>
2020-01-18 16:52:43 -0400

    creating default project structure

---
+++ src/test/java/com/ffguerrero/java/springboot/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffguerrero.java.springboot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
